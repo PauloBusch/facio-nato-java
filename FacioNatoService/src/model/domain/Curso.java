c4fef2e58c88851850488dfff7fe7fb970c41939
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-06-18 21:04:24 -0300

    Classe Curso

---
+++ FacioNatoService/src/model/domain/Curso.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.domain;
+
+/**
+ *
+ * @author paulo
+ */
+public class Curso {
+    private String nome;
+
+    public Curso() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
